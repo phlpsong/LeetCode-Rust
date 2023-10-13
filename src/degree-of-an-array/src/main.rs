b1cd46be0809f39e592e333cb2caa3d46d50e81d
phlpsong <philsongfei@gmail.com>
2023-10-13 21:38:35 +0800

    update: optimize problem 697

--- src/degree-of-an-array/src/main.rs
+++ src/degree-of-an-array/src/main.rs
@@ -9,24 +9,19 @@ struct Solution { }
 impl Solution {
     pub fn find_shortest_sub_array(nums: Vec<i32>) -> i32 {
         let mut map: HashMap<i32, Vec<i32>> = HashMap::new();
+        let mut degree = 0;
         for index in 0..nums.len() {
-            if let Some(val) = map.get(&nums[index]) {
-                map.insert(nums[index], vec![val[0] + 1, val[1], index as i32]);
-            } else {
-                map.insert(nums[index], vec![1, index as i32, index as i32]);
-            }
+            let cell = map.entry(nums[index]).or_insert(vec![0, index as i32, 1]);
+            cell[0] = cell[0] + 1;
+            cell[2] = (index as i32) - cell[1] + 1;
+            degree = degree.max(cell[0]);
         }
-        let mut degree = 1;
-        let mut count = i32::MAX;
-        for (&_key, &ref val) in map.iter() {
-            if val[0] > degree {
-                degree = val[0];
-                count = val[2] - val[1];
-            } else if val[0] == degree {
-                count = count.min(val[2] - val[1]);
-            }
-        }
-        return count + 1;
+        
+        return map.values()
+            .filter(|cell| cell[0] == degree)
+            .map(|cell| cell[2])
+            .min()
+            .unwrap() as i32;
     }
 }
 
